f3df6a7c10a167080f7f73612bbfe6f6ed866d28
ConnorBS <connor.b.stewart.36@gmail.com>
2021-11-03 22:21:45 -0400

    Knight Character seperate scene + add Camera

--- Scripts/PrototypeRoom.gd
+++ Scripts/PrototypeRoom.gd
@@ -15,14 +15,14 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 
 ##Checks input and applies force to background to move background and keep character
-func _process(delta):
-	if Input.is_action_pressed("ui_right"):
-		backdrop.add_force(backdrop.position, Vector2(-1,0))
-#		if backdrop.applied_force <= Vector2(-max_movement_speed,0):
-#			backdrop.applied_force = Vector2(-max_movement_speed,0)
-	elif Input.is_action_pressed("ui_left"):
-		backdrop.add_force(backdrop.position, Vector2(1,0))
-#		if backdrop.applied_force >= Vector2(max_movement_speed,0):
-#			backdrop.applied_force = Vector2(max_movement_speed,0)
-	print (backdrop.applied_force)
-#	pass
+#func _process(delta):
+#	if Input.is_action_pressed("ui_right"):
+#		backdrop.add_force(backdrop.position, Vector2(-1,0))
+##		if backdrop.applied_force <= Vector2(-max_movement_speed,0):
+##			backdrop.applied_force = Vector2(-max_movement_speed,0)
+#	elif Input.is_action_pressed("ui_left"):
+#		backdrop.add_force(backdrop.position, Vector2(1,0))
+##		if backdrop.applied_force >= Vector2(max_movement_speed,0):
+##			backdrop.applied_force = Vector2(max_movement_speed,0)
+#	print (backdrop.applied_force)
+##	pass
