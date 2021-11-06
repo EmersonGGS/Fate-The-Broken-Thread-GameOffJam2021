01ae5c83257cb2b610d61eeccf3a88f162c296c9
Emerson <emersongstewart@gmail.com>
2021-11-06 14:00:37 -0400

    Animated left and right run

--- Scenes/AnimatedSprite.gd
+++ Scenes/AnimatedSprite.gd
@@ -1,9 +1,5 @@
 extends AnimatedSprite
 
-func _ready() -> void:
-	var instanceObject = SpriteFrames.new()
-	$AnimatedSprite.play("Idle")
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -7,6 +7,7 @@ export var speed = Vector2(300.0, 1000.0)
 export var gravity = 1.0;
 const JUMPFORCE = -1200;
 
+onready var animatedSprite = $AnimatedSprite
 
 func _physics_process(delta: float) -> void:
 		var direction = getDirection();
@@ -16,10 +17,30 @@ func _physics_process(delta: float) -> void:
 		velocity = move_and_slide(velocity, Vector2.UP)
 
 func getDirection() -> Vector2:
-	return Vector2(
-		Input.get_action_strength("right") - Input.get_action_strength("left"),
-		-1.0
-	)
+	# Movement direction of player, < 0 is moving right, 0 is stationary, < 0 is moving left
+	var direction = Input.get_action_strength("right") - Input.get_action_strength("left")
+	
+	# Handle animation direction
+	# Running right
+	if direction > 0 :
+		animatedSprite.animation = "Running"
+		animatedSprite.flip_h = false
+
+	# Running left
+	elif direction < 0:
+		animatedSprite.animation = "Running"
+		animatedSprite.flip_h = true
+
+	# Not moving
+	else:
+		animatedSprite.animation = "Idle"
+
+	# If animated sprite is not playing, play it
+	if animatedSprite.playing == false:
+		animatedSprite.play()
+		
+	# Update the direction
+	return Vector2(direction, -1.0)
 
 func calculate_move_velocity(linear_velocity: Vector2, speed: Vector2, direction: Vector2) -> Vector2:
 	var calculated_velocity = linear_velocity
@@ -27,5 +48,5 @@ func calculate_move_velocity(linear_velocity: Vector2, speed: Vector2, direction
 	calculated_velocity.y += gravity * get_physics_process_delta_time()
 	if direction.y == -1.0:
 		calculated_velocity.y = speed.y * direction.y
-	print(calculated_velocity)
+
 	return calculated_velocity
