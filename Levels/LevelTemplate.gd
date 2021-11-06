b5af814390143d6171db3544640a18f5029a43f5
ConnorBS <connor.b.stewart.36@gmail.com>
2021-11-06 13:11:38 -0400

    slime

---
+++ Levels/LevelTemplate.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+
+#array of enemy types to have available to load (could also be used 
+onready var enemies = [preload("res://Scenes/Enemy.tscn").instance()]
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var slime = enemies[0].duplicate()
+	add_child(slime);
+	slime.position = Vector2(300,500)
+	pass # Replace withfunction body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
